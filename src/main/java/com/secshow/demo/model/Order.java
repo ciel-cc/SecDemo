534d533bd647f9b168e0d9e9feced2d95153b668
ciel-cc <cielcmh@163.com>
2020-04-09 09:39:00 +0800

    fix: 修改表order字段

--- src/main/java/com/secshow/demo/model/Order.java
+++ src/main/java/com/secshow/demo/model/Order.java
@@ -9,6 +9,10 @@ import java.util.Date;
 public class Order {
     private Integer id;
 
+    private BigDecimal stuffprice;
+
+    private BigDecimal freightprice;
+
     private BigDecimal totalprice;
 
     private String statu;
@@ -43,117 +47,5 @@ public class Order {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public BigDecimal getTotalprice() {
-        return totalprice;
-    }
-
-    public void setTotalprice(BigDecimal totalprice) {
-        this.totalprice = totalprice;
-    }
-
-    public String getStatu() {
-        return statu;
-    }
-
-    public void setStatu(String statu) {
-        this.statu = statu == null ? null : statu.trim();
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name == null ? null : name.trim();
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address == null ? null : address.trim();
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone == null ? null : phone.trim();
-    }
-
-    public String getPostmethod() {
-        return postmethod;
-    }
-
-    public void setPostmethod(String postmethod) {
-        this.postmethod = postmethod == null ? null : postmethod.trim();
-    }
-
-    public Date getOrderTime() {
-        return orderTime;
-    }
-
-    public void setOrderTime(Date orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Integer getProId() {
-        return proId;
-    }
-
-    public void setProId(Integer proId) {
-        this.proId = proId;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Integer getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(Integer createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(Integer updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Integer getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Integer isDeleted) {
-        this.isDeleted = isDeleted;
     }
 }
